c61f56a3f72dfc4cdefefef28f8a7663c1bbb0c3
¨kevin180317¨ <ortega1gv@gmail.com>
2025-01-18 12:55:30 -0800

    update

---
+++ src/components/Icons.jsx
@@ -0,0 +1,17 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
+function Icons() {
+  return (
+    <div class="flex gap-4 px-4 py-4">
+        <FaFacebook className='text-2xl hover:text-blue-500 hover:cursor-pointer' />
+        <FaInstagram className='text-2xl hover:text-blue-500 hover:cursor-pointer' />
+        <FaXTwitter className='text-2xl hover:text-blue-500 hover:cursor-pointer' />
+        <FaLinkedin className='text-2xl hover:text-blue-500 hover:cursor-pointer' />
+    </div>
+  )
+}
+
+export default Icons
