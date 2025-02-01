31abd78e18a3f27e3a8b901edc0959409a2a8966
¨kevin180317¨ <ortega1gv@gmail.com>
2025-02-01 11:05:46 -0800

    update

--- src/components/prices/Product.jsx
+++ src/components/prices/Product.jsx
@@ -43,7 +43,7 @@ export default function Product({ categoria}) {
     const webProjects = projects.filter(project => project.category ===  categoria);
 
     return (
-<div className="p-8 min-h-screen bg-gray-50">
+<div className="p-8 bg-gray-50">
     <div className="max-w-7xl mx-auto">
         {/* Grid de proyectos mejorado con efecto de panel deslizable */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
